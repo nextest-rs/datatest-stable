acdebc749fae16e6fcad957a71d7faf555520a05
sunshowers <rain@sunshowers.io>
2022-05-22 22:38:32 +0000

    Deploying to gh-pages from  @ 5e56903d13484edda341e5ee9ddc4f5b2d78b08c 🚀

---
+++ rustdoc/implementors/clap/derive/trait.CommandFactory.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libtest_mimic"] = [{"text":"impl <a class=\"trait\" href=\"clap/derive/trait.CommandFactory.html\" title=\"trait clap::derive::CommandFactory\">CommandFactory</a> for <a class=\"struct\" href=\"libtest_mimic/struct.Arguments.html\" title=\"struct libtest_mimic::Arguments\">Arguments</a>","synthetic":false,"types":["libtest_mimic::args::Arguments"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ rustdoc/implementors/clap/derive/trait.Parser.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["libtest_mimic"] = [{"text":"impl <a class=\"trait\" href=\"clap/derive/trait.Parser.html\" title=\"trait clap::derive::Parser\">Parser</a> for <a class=\"struct\" href=\"libtest_mimic/struct.Arguments.html\" title=\"struct libtest_mimic::Arguments\">Arguments</a>","synthetic":false,"types":["libtest_mimic::args::Arguments"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
