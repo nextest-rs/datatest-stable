be03944bba05d77262f1568be627b5024143b001
sunshowers <rain@sunshowers.io>
2022-08-15 15:34:03 +0000

    Deploying to gh-pages from  @ bdd792748e207cf06c4a180566de50da68276ff8 🚀

--- rustdoc/implementors/clap/derive/trait.Args.js
+++ rustdoc/implementors/clap/derive/trait.Args.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["clap"] = [];
 implementors["libtest_mimic"] = [{"text":"impl <a class=\"trait\" href=\"clap/derive/trait.Args.html\" title=\"trait clap::derive::Args\">Args</a> for <a class=\"struct\" href=\"libtest_mimic/struct.Arguments.html\" title=\"struct libtest_mimic::Arguments\">Arguments</a>","synthetic":false,"types":["libtest_mimic::args::Arguments"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
